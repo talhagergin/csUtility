bb0601abb8da42489d3c3a3b13e5acb35ce440d8
Talha Gergin <t.gergin@simplydeliver.com>
2025-05-16 18:59:40 +0300

    Initial Commit

---
+++ csUtility/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  csUtility
+//
+//  Created by Talha Gergin on 16.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ csUtility/csUtilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  csUtilityApp.swift
+//  csUtility
+//
+//  Created by Talha Gergin on 16.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct csUtilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
